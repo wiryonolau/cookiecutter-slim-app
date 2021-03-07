4149410d44a04090f3325a24855a9c051dc80625
Wiryono <wiryonolau@gmail.com>
2021-03-07 13:55:27 +0700

    Add body parsing middleware

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/config/middleware.config.php
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/config/middleware.config.php
@@ -5,8 +5,9 @@ namespace {{ cookiecutter.project_namespace }};
 
 use Itseasy;
 
-use Slim\Middleware\RoutingMiddleware;
+use Slim\Middleware\BodyParsingMiddleware;
 use Slim\Middleware\ErrorMiddleware;
+use Slim\Middleware\RoutingMiddleware;
 
 # Order is important
 return [
@@ -14,6 +15,7 @@ return [
         "middleware" => [
             Itseasy\Csrf\CsrfMiddleware::class,
             Itseasy\Session\SessionMiddleware::class,
+            BodyParsingMiddleware::class,
             RoutingMiddleware::class,
             Itseasy\Asset\AssetMiddleware::class,
             Itseasy\Http\HttpExceptionMiddleware::class,
