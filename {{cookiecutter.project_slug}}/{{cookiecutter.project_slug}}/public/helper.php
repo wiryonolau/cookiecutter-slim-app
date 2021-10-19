c0cbd88543925c3be125dc8953f8736da3308220
Wiryono <wiryonolau@gmail.com>
2021-10-19 11:50:22 +0700

    Update helper.php

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/public/helper.php
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/public/helper.php
@@ -12,11 +12,43 @@ function debug($value, bool $halt = false, ?string $label = null)
         echo "\n";
     } else {
         echo '<pre>';
-        print_r($value);
+        echo htmlspecialchars(print_r($value, true));
         echo '</pre>';
     }
 
     if ($halt) {
         die();
     }
+}
+
+function get_docker_secret($path, $default = "", bool $path_from_env = false)
+{
+    if ($path_from_env) {
+        $path = getenv($path);
+    }
+
+    if (is_string($path) === false) {
+        return $default;
+    }
+
+    if (file_exists($path) === false) {
+        return $default;
+    }
+
+    $secret = file_get_contents($path);
+    if ($secret === false) {
+        return $default;
+    }
+
+    return trim($secret);
+}
+
+function get_env(string $name, $default = "", bool $local_only = false)
+{
+    $env = getenv($name, $local_only);
+    if ($env === false || is_null($env)) {
+        $env = $default;
+    }
+
+    return $env;
 }
