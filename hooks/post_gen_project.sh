346da309b973e8fde285fa97f689afcd01fae83e
Wiryono Lauw <wiryonolau@gmail.com>
2022-04-11 21:10:51 +0700

    Add babel script transpiler, .languagebabel config for atom ide only

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,6 +2,6 @@
 git init
 make composer-install
 make composer-update
-make yarn install
+make yarn-install
 cp docker-compose.nginx-sample.yml docker-compose.yml
 cp docker-compose.sample.env docker-compose.env
